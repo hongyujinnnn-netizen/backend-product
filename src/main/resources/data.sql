6afeef4213a14d19ae81dd9f11590fb93cf6e940
Kong Hong <hong.yujinnnn@icloud.com>
2026-01-26 22:09:21 +0700

    fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-
+-- Seed admin user and a few products. Password hash corresponds to Admin123!
+INSERT INTO users (username, email, password, role)
+SELECT 'admin', 'ryu@mail.com', '$2a$10$7b7PjdvVS.wGInxjeRGnaO0Jsbgx5caX5/C/PObbIVdQydb9h9t7C', 'ADMIN'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin');
